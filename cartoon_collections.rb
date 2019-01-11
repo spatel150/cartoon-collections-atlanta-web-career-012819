915b41b03af0c65bf939b14d770b8dfad3eff95a
Samidh Patel <spatel13456@gmail.com>
2019-01-11 20:45:48 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,7 +20,7 @@ def long_planeteer_calls(short_words)
   end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(cheese)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  if (cheese )
 end
